083c27e37ba39d7b9ca45b589513cffc27db3f2d
fnixdev <luisgatn000@gmail.com>
2022-07-01 12:00:00 -0300

    @fnixdev

--- plugins/kawaii/wttr/__main__.py
+++ plugins/kawaii/wttr/__main__.py
@@ -4,8 +4,7 @@
 #
 # ==
 
-import asyncio
-import requests
+import aiohttp
 
 from html import escape
 
@@ -29,13 +28,20 @@ async def weather_get(message: Message):
     location = message.input_str
     if not location:
         return await message.err("`Especifique uma cidade.\n.Ex: .w Brasilia`", del_in=5)
-    try:
-        resp = requests.get(f"https://wttr.in/{location}?mnTC0&lang=pt-br")
-        data = await resp.text()
-    except Exception:
-        return await message.err("`Falha ao obter weather deasa cidade.`", del_in=5)
-    if "we processed more than 1M requests today" in data:
-        await message.edit("`Você fez muitas requisições, espere ate amanhã e tente novamente!`")
+    if len(location) > 1:
+        headers = {"user-agent": "httpie"}
+        url = f"https://wttr.in/{location}?mnTC0&lang=pt-br"
+        try:
+            async with aiohttp.ClientSession(headers=headers) as session:
+                async with session.get(url) as resp:
+                    data = await resp.text()
+        except Exception:
+            return await message.err("`Falha ao obter weather deasa cidade.`", del_in=5)
+
+        if "we processed more than 1M requests today" in data:
+            await message.edit("`Você fez muitas requisições, espere ate amanhã e tente novamente!`")
+        else:
+            weather = f"<code>{escape(data.replace('report', 'Report'))}</code>"
+            await message.edit(weather, parse_mode=ParseMode.HTML)
     else:
-        weather = f"<code>{escape(data.replace('report', 'Report'))}</code>"
-        await message.edit(weather, parse_mode=ParseMode.HTML)
+        await message.delete()
