8df99faba837f5d866ec23d80223ecd96f89d0ed
fnixdev <luisgatn000@gmail.com>
2022-07-01 22:18:44 -0300

    @fnixdev

--- plugins/utils/wallpaper/__main__.py
+++ plugins/utils/wallpaper/__main__.py
@@ -6,9 +6,7 @@
 #
 # ==
 
-
 import requests
-import wget
 
 from userge import userge, Message, pool
 
@@ -23,8 +21,7 @@ from userge import userge, Message, pool
     'usage': "{tr}wall [Query]",
     'examples': "{tr}wall kanna"})
 async def wall_(msg: Message):
-    limit = min(int(msg.flags.get('-l', 8)), 10)
-    wall = []
+    limit = min(int(msg.flags.get('-l', 1)), 10)
     if msg.filtered_input_str:
         qu = msg.filtered_input_str
         await msg.edit(f"__searching wallpapers__ ... `{qu}`")
@@ -33,8 +30,10 @@ async def wall_(msg: Message):
             if results.status_code != 200:
                 return await msg.edit('**Result Not Found**')
             _json = results.json()['url']
-            wall.append(_json)
-        await msg.edit(wall)
+            if '-doc' in msg.flags:
+                await msg.send_document(_json)
+            else:
+                await msg.send_photo(_json)
     else:
         await msg.edit('**Give me Something to search.**')
         await msg.reply_sticker('CAADAQADmQADTusQR6fPCVZ3EhDoFgQ')
