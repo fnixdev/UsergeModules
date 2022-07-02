3c694d2f4ebb7fb6917cb6510d843f8008e57c34
fnixdev <luisgatn000@gmail.com>
2022-07-01 22:37:52 -0300

    @fnixdev

--- plugins/utils/wallpaper/__main__.py
+++ plugins/utils/wallpaper/__main__.py
@@ -25,11 +25,44 @@ async def wall_(msg: Message):
     if msg.filtered_input_str:
         qu = msg.filtered_input_str
         await msg.edit(f"__searching wallpapers__ ... `{qu}`")
+        await msg.delete()
         for i in range(limit):
             results = requests.get(f"https://kuuhaku-api-production.up.railway.app/api/wallpaper?query={qu}")
             if results.status_code != 200:
                 return await msg.edit('**Result Not Found**')
-            msg.delete()
+            if "error" in results.json():
+                return await msg.edit(f"{qu} don't have wallpapers")
+            _json = results.json()['url']
+            if '-doc' in msg.flags:
+                await msg.client.send_document(msg.chat.id, document=_json)
+            else:
+                await msg.client.send_photo(msg.chat.id, photo=_json)
+    else:
+        await msg.edit('**Give me Something to search.**')
+        await msg.reply_sticker('CAADAQADmQADTusQR6fPCVZ3EhDoFgQ')
+
+
+@userge.on_cmd("wall", about={
+    'header': "Search Mobile Wallpaper",
+    'flags': {
+        '-l': "Limit of Wallpapers",
+        '-doc': "Send as Documents (Recommended)"
+    },
+    'description': 'Search and Download Hd Wallpaper from AlphaCoders and upload to Telegram',
+    'usage': "{tr}mwall [Query]",
+    'examples': "{tr}mwall kanna"})
+async def wall_(msg: Message):
+    limit = min(int(msg.flags.get('-l', 1)), 10)
+    if msg.filtered_input_str:
+        qu = msg.filtered_input_str
+        await msg.edit(f"__searching wallpapers__ ... `{qu}`")
+        await msg.delete()
+        for i in range(limit):
+            results = requests.get(f"https://kuuhaku-api-production.up.railway.app/api/wallpaper?query={qu}")
+            if results.status_code != 200:
+                return await msg.edit('**Result Not Found**')
+            if "error" in results.json():
+                return await msg.edit(f"{qu} don't have mobile wallpapers")
             _json = results.json()['url']
             if '-doc' in msg.flags:
                 await msg.client.send_document(msg.chat.id, document=_json)
