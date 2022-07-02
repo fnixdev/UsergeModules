96c64c0816bff9275b12a18b01717748dd44ec63
fnixdev <luisgatn000@gmail.com>
2022-07-01 22:13:32 -0300

    @fnixdev

--- plugins/utils/wallpaper/__main__.py
+++ plugins/utils/wallpaper/__main__.py
@@ -6,16 +6,9 @@
 #
 # ==
 
-# rewrite module by @Krishna_Singhal
-
-import os
-import shutil
 
 import requests
 import wget
-from PIL import Image
-from pyrogram.enums import ChatAction
-from pyrogram.types import InputMediaPhoto, InputMediaDocument
 
 from userge import userge, Message, pool
 
@@ -26,57 +19,22 @@ from userge import userge, Message, pool
         '-l': "Limit of Wallpapers",
         '-doc': "Send as Documents (Recommended)"
     },
-    'description': 'Search and Download Hd Wallpaper from Unsplash and upload to Telegram',
+    'description': 'Search and Download Hd Wallpaper from AlphaCoders and upload to Telegram',
     'usage': "{tr}wall [Query]",
-    'examples': "{tr}wall luffy"})
+    'examples': "{tr}wall kanna"})
 async def wall_(msg: Message):
-
-    if os.path.exists("wallpapers/"):
-        shutil.rmtree("wallpapers/", ignore_errors=True)
-
     limit = min(int(msg.flags.get('-l', 8)), 10)
-
+    wall = []
     if msg.filtered_input_str:
         qu = msg.filtered_input_str
         await msg.edit(f"__searching wallpapers__ ... `{qu}`")
-        results = requests.get(
-            "https://api.unsplash.com/search/"
-            f"photos?client_id=HWlOs9dNZIbYEkjp87fiEzC9rmE6rKM64tBqXBOLzu8&query={qu}"
-        )
-
-        if results.status_code != 200:
-            return await msg.edit('**Result Not Found**')
-        _json = results.json()['results']
-        if len(_json) < limit:
-            limit = len(_json)
-
-        ss = []
-        os.mkdir("wallpapers")
-
         for i in range(limit):
-            img = f"wallpapers/wall_{i+1}.png"
-
-            if '-doc' in msg.flags:
-                await pool.run_in_thread(wget.download)(_json[i]['urls']['raw'], img)
-                ss.append(InputMediaDocument(str(img)))
-                continue
-
-            await pool.run_in_thread(wget.download)(_json[i]['urls']['thumb'], img)
-            image = Image.open(img)
-            if not (image.height <= 1280 and image.width <= 1280):
-                image.thumbnail((1280, 1280), Image.ANTIALIAS)
-                a_dex = image.mode.find("A")
-                if a_dex != -1:
-                    new_im = Image.new('RGB', image.size, (255, 255, 255))
-                    new_im.paste(image, mask=image.split()[a_dex])
-                    new_im.save(img, 'JPEG', optimize=True)
-            ss.append(InputMediaPhoto(str(img)))
-
-        await msg.reply_chat_action(
-            ChatAction.UPLOAD_DOCUMENT if '-doc' in msg.flags else ChatAction.UPLOAD_PHOTO)
-        await msg.reply_media_group(ss, True)
-        shutil.rmtree("wallpapers/", ignore_errors=True)
-        await msg.delete()
+            results = requests.get(f"https://kuuhaku-api-production.up.railway.app/api/wallpaper?query={qu}")
+            if results.status_code != 200:
+                return await msg.edit('**Result Not Found**')
+            _json = results.json()['url']
+            wall.append(_json)
+        await msg.edit(wall)
     else:
         await msg.edit('**Give me Something to search.**')
         await msg.reply_sticker('CAADAQADmQADTusQR6fPCVZ3EhDoFgQ')
